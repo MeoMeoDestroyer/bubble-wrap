c6583c03677376696f04ec2cd7cb15e288f42b6f
Kate_nguyen <253353253+MeoMeoDestroyer@users.noreply.github.com>
2026-01-14 01:09:57 +0000

    change color when press a

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,17 @@
 #include <bn_backdrop.h>
 #include <bn_color.h>
 #include <bn_core.h>
+#include <bn_keypad.h>
 
 int main() {
+  
     bn::core::init();
     bn::backdrop::set_color(bn::color(21, 15, 15));
+
     while(true) {
+        if (bn::keypad::a_pressed()) {
+    bn::backdrop::set_color(bn::color(31, 21, 22));
+    }
     bn::core::update();
 }
 }
